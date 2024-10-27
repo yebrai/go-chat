cbcecd961ac2d8296139382a1af0fb292b834ad7
yebrai <ivansevv@gmail.com>
2024-10-27 13:11:27 +0100

    Fix: Corrected the route for serving JavaScript files

--- internal/server/server.go
+++ internal/server/server.go
@@ -5,10 +5,16 @@ import (
 	"net/http"
 )
 
+// New creates and configures a new HTTP server
 func New() *http.ServeMux {
 	mux := http.NewServeMux()
+	// Sirve archivos estáticos directamente desde la carpeta "static"
 	mux.Handle("/", http.FileServer(http.Dir("./web/static")))
+	// Configura la ruta correcta para los archivos en "assets/js"
+	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./web/assets"))))
+	// WebSocket handler
 	mux.HandleFunc("/ws", websocket.HandleConnections)
+	// Manejo de mensajes concurrentes para WebSocket
 	go websocket.HandleMessages()
 	return mux
 }
